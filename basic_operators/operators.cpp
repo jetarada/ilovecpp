c8ff99a3fd6707bebdf3fd722fd3918b016a2787
Jet Arada <45038186+jetarada@users.noreply.github.com>
2025-10-15 21:31:03 +0800

    Add files via upload
    c++ basic operators

---
+++ basic_operators/operators.cpp
@@ -0,0 +1,25 @@
+// Include Files
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Variable sections
+    int a = 6;
+    int b = 3; 
+
+    // Add 'a' and 'b'
+    cout << a << " + " << b << " = " << a + b << endl;
+
+    // Subtract 'a' and 'b'
+    cout << a << " - " << b << " = " << a - b << endl;
+
+    // Multiply 'a' and 'b'
+    cout << a << " x " << b << " = " << a * b << endl;
+
+    // Divide 'a' and 'b'
+    cout << a << " / " << b << " = " << a / b << endl;
+
+    return 0;
+
+}
