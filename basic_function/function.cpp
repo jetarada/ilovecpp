13cb6396e9478944e2330806d36b9415126a7b0f
Jet Arada <45038186+jetarada@users.noreply.github.com>
2025-10-15 22:08:22 +0800

    Add files via upload

---
+++ basic_function/function.cpp
@@ -0,0 +1,19 @@
+// Include Files
+#include <iostream>
+using namespace std;
+
+// Function prototype
+int sum(int x, int y) {
+
+    // Function Definition
+    return x + y;
+
+}
+
+int main() {
+
+    // Function Call
+    cout << "The sum of 3 and 4 is " << sum (3, 4) << endl;
+    return 0;
+    
+}
