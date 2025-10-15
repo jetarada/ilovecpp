c10be97cf7b0e0d767e9934b0c70cdd2cff3630e
Jet Arada <45038186+jetarada@users.noreply.github.com>
2025-10-15 22:10:28 +0800

    Add files via upload

---
+++ parameter_argument/para_argu.cpp
@@ -0,0 +1,18 @@
+// Include file
+#include <iostream>
+using namespace std;
+
+// Function Prototype and Definition
+void myFunction(string fname) {
+    cout << fname << "\n";
+}
+
+int main() {
+    // Function Calls
+    myFunction("Liam");     // Function call
+    myFunction("Jenny");     // Function call
+    myFunction("Anja");
+    return 0;
+}
+
+
